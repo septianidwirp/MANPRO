0c0551bb6e7a0583936621598a0b043be62b4059
Septigooooo <20081010177@student.upnjatim.ac.id>
2023-11-02 17:53:39 +0700

    feat : update login & register page

--- app.py
+++ app.py
@@ -28,3 +28,7 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/riwayat')
+def riwayat():
+    return render_template('riwayat.html')
